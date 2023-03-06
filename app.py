fe695b477ce05022ac520871b922ee254ae32e04
barryjung <122099543+barryjung@users.noreply.github.com>
2023-03-06 14:40:32 +0900

    app.py 수정
    기능 추가
    - 메인페이지
    - 상세페이지 이동
    - 맛집 추가하기 기능

--- app.py
+++ app.py
@@ -7,25 +7,30 @@ db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index_main.html')
 
-@app.route("/example", methods=["POST"])
-def mars_post():
-    title_receive = request.form['title_give']
-    text_receive = request.form['text_give']
+@app.route('/detail')
+def detail():
+    return render_template('index_detail.html')
+
+@app.route("/save", methods=["POST"])
+def mglist_add():
+    image_receive = request.form['image_give']
+    shop_receive = request.form['shop_give']
+    place_receive = request.form['place_give']
+    menu_receive = request.form['menu_give']
+    star_receive = request.form['star_give']
 
     doc = {
-        'title':title_receive,
-        'text':text_receive
+        'image':image_receive,
+        'shop':shop_receive,
+        'place':place_receive,
+        'menu':menu_receive,
+        'star':star_receive
         }
     db.mglist.insert_one(doc)
 
     return jsonify({'msg':'저장완료!'})
-
-@app.route("/example", methods=["GET"])
-def mars_get():
-    list_data = list(db.mglist.find({},{'_id':False}))
-    return jsonify({'result':list_data})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
