18529a94ebfdbc0091b375db59de56350f0c51a7
barryjung <122099543+barryjung@users.noreply.github.com>
2023-03-07 16:22:29 +0900

    app.py, main, detail수정
    메인페이지 정보불러오기
    상세페이지 정보불러오기

--- app.py
+++ app.py
@@ -11,21 +11,35 @@ def home():
 
 @app.route('/detail')
 def detail():
+    mglist = db.mglist.find_one({'shop':'맛집'},{'_id':False})
+    print(mglist)
     return render_template('index_detail.html')
+
+@app.route("/sik", methods=["GET"])
+def mglist_get():
+    sikdangs_list = list(db.mglist.find({},{'_id':False}))
+    return jsonify({'mglist':sikdangs_list})
+
+@app.route("/detail/get", methods=["GET"])
+def detail_get():
+    mglist = db.mglist.find_one({'shop':'맛집'},{'_id':False})
+    print(mglist)
+    # return render_template('index_detail.html')
+    return jsonify({'result': mglist})
 
 @app.route("/save", methods=["POST"])
 def mglist_add():
     image_receive = request.form['image_give']
     shop_receive = request.form['shop_give']
-    place_receive = request.form['place_give']
     menu_receive = request.form['menu_give']
+    place_receive = request.form['place_give']
     star_receive = request.form['star_give']
 
     doc = {
         'image':image_receive,
         'shop':shop_receive,
-        'place':place_receive,
         'menu':menu_receive,
+        'place':place_receive,
         'star':star_receive
         }
     db.mglist.insert_one(doc)
