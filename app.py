a2bfc691500f2f8abc9175abdb5b45b027411c2c
JooHan10 <juhanlee2218@gmail.com>
2023-03-08 12:28:04 +0900

    디자인 수정 펼치기 가능 슬라이드 7번 클릭까지만 가능하게 수정
    디자인 수정 펼치기 가능 슬라이드 7번 클릭까지만 가능하게 수정 했습니다!

--- app.py
+++ app.py
@@ -1,31 +1,37 @@
+from pymongo import MongoClient
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://sparta:test@cluster0.cqki075.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient(
+    'mongodb+srv://sparta:test@cluster0.cqki075.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
+
 
 @app.route('/')
 def home():
     return render_template('index_main.html')
 
+
 @app.route('/detail')
 def detail():
-    mglist = db.mglist.find_one({'shop':'맛집'},{'_id':False})
+    mglist = db.mglist.find_one({'shop': '맛집'}, {'_id': False})
     print(mglist)
     return render_template('index_detail.html')
 
+
 @app.route("/sik", methods=["GET"])
 def mglist_get():
-    sikdangs_list = list(db.mglist.find({},{'_id':False}))
-    return jsonify({'mglist':sikdangs_list})
+    sikdangs_list = list(db.mglist.find({}, {'_id': False}))
+    return jsonify({'mglist': sikdangs_list})
+
 
 @app.route("/detail/<id>", methods=["GET"])
 def detail_get(id):
-    mglist = db.mglist.find_one({'shop':id},{'_id':False})
+    mglist = db.mglist.find_one({'shop': id}, {'_id': False})
     print(mglist)
     return render_template('index_detail.html', data=mglist)
     # return jsonify({'result': mglist})
+
 
 @app.route("/save", methods=["POST"])
 def mglist_add():
@@ -36,15 +42,16 @@ def mglist_add():
     star_receive = request.form['star_give']
 
     doc = {
-        'image':image_receive,
-        'shop':shop_receive,
-        'menu':menu_receive,
-        'place':place_receive,
-        'star':star_receive
-        }
+        'image': image_receive,
+        'shop': shop_receive,
+        'menu': menu_receive,
+        'place': place_receive,
+        'star': star_receive
+    }
     db.mglist.insert_one(doc)
 
-    return jsonify({'msg':'저장완료!'})
+    return jsonify({'msg': '저장완료!'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
